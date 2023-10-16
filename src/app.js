ce2ddf15a89c9f8bcba5270081d6311b941cdad8
Luis Fuentealba Pérez <lfuentealbap@outlook.cl>
2023-10-16 17:59:59 -0300

    Integración de CORS

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const authRoutes = require('./routes/user.router');
 const conversionRoutes = require('./routes/conversion.router');
 
 const port = process.env.PORT || 3000;
-
+app.use(cors());
+app.options('*', cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('/api', authRoutes);
